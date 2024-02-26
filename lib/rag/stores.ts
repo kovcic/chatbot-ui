f9650fafa16b22dfbecc4c9f6d45f56c4773cc04
Andrej Kovcic <kovcic@gmail.com>
2024-02-26 15:39:08 +0100

    add mongodb, qdrant config

--- lib/rag/stores.ts
+++ lib/rag/stores.ts
@@ -1,15 +1,12 @@
-import {
-  QdrantVectorStore,
-  SimpleDocumentStore,
-  SimpleIndexStore
-} from "llamaindex"
+import { QdrantVectorStore } from "llamaindex"
 import { MongoKVStore } from "./store/mongo-kvstore"
 import KVDocumentStore from "./store/kv-document-store"
 import { KVIndexStore } from "./store/kv-index-store"
 
 export const createVectorStore = (collectionName: string) => {
   const vectorStore = new QdrantVectorStore({
-    url: "http://localhost:6333",
+    url: process.env.QDRANT_API_URL,
+    apiKey: process.env.QDRANT_API_KEY,
     collectionName
   })
 
@@ -17,14 +14,14 @@ export const createVectorStore = (collectionName: string) => {
 }
 
 export const createDocumentStore = async (collectionName: string) => {
-  const kvstore = new MongoKVStore("mongodb://localhost:27017", "docstore")
+  const kvstore = new MongoKVStore(process.env.MONGO_DB_URI!, "docstore")
   const store = new KVDocumentStore(kvstore, collectionName)
 
   return store
 }
 
 export const createIndexStore = async (collectionName: string) => {
-  const kvstore = new MongoKVStore("mongodb://localhost:27017", "index_store")
+  const kvstore = new MongoKVStore(process.env.MONGO_DB_URI!, "index_store")
   const store = new KVIndexStore(kvstore, collectionName)
 
   return store
