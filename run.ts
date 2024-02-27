55e1a774b3ab3ad561ab5ea74906b36234de892d
Andrej Kovcic <kovcic@gmail.com>
2024-02-27 16:33:33 +0100

    fix for summary index creation

--- run.ts
+++ run.ts
@@ -1,20 +1,22 @@
+import 'dotenv/config';
 import fs from'node:fs';
-import { MarkdownNodeParser, SimpleDirectoryReader, SimpleNodeParser, Document } from 'llamaindex';
+import { MarkdownNodeParser, SimpleDirectoryReader, SimpleNodeParser, Document, ServiceContext } from 'llamaindex';
 import { extractMetadata } from './lib/rag/metadata';
 import { createVectorIndex } from './lib/rag/vector-index';
-import { createSummaryIndex, getSummaryIndex } from './lib/rag/summary-index';
+import { createSummaryIndex, deleteSummaryIndex, getSummaryIndex } from './lib/rag/summary-index';
 import { createDocumentAgent, createTopAgent } from './lib/rag/agents';
 import * as llamaParse from './lib/rag/llama-parse';
 import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
 import { MongoKVStore } from './lib/rag/store/mongo-kvstore';
 import KVDocumentStore from './lib/rag/store/kv-document-store';
 import { KVIndexStore } from './lib/rag/store/kv-index-store';
+import { createServiceContext } from './lib/rag/service-context';
 
 const reader = new SimpleDirectoryReader();
 const documents = await reader.loadData('/Users/andrej.kovcic/dev/ds/spark-pilot/example-docs/samples4');
 
 const run = async () => {
-  const file = fs.readFileSync('/Users/andrej.kovcic/dev/ds/spark-pilot/example-docs/samples4/bcce52d8-7202-4217-8f86-8523b96c9755.md');
+  const file = fs.readFileSync('/Users/andrej.kovcic/dev/ds/spark-pilot/example-docs/samples2/embedded-images-tables.md');
   const text = file.toString('utf8');
   const documents = [new Document({ text })];
 
@@ -50,8 +52,13 @@ const run = async () => {
   // const response = await queryEngine.query({ query: 'what is H2SO4?' });
   // console.log(response);
 
-  // const summaryIndex = await createSummaryIndex('test', documents);
+  const serviceContext: ServiceContext = {
+    ...createServiceContext(),
+    nodeParser: MarkdownNodeParser.fromDefaults(),
+  };
+  // const summaryIndex = await createSummaryIndex('test', documents, { serviceContext });
   // const summaryIndex = await getSummaryIndex('test');
+  // deleteSummaryIndex('test')
   // const queryEngine = summaryIndex.asQueryEngine();
   // const response = await queryEngine.query({ query: 'how the presence of ES affects to corrosion of stainless steal?' });
   // console.log(response.response);
@@ -59,11 +66,11 @@ const run = async () => {
   // const metadata = await extractMetadata(nodes);
   // console.info(metadata);
 
-  const agent = await createDocumentAgent('424eea88-7d36-4b35-8d94-1583864980b1', 'Langmuir Adsorption Isotherm and Corrosion Inhibition of Stainless Steel by Egg Shell in Sulphuric Acid Medium');
+  // const agent = await createDocumentAgent('424eea88-7d36-4b35-8d94-1583864980b1', 'Langmuir Adsorption Isotherm and Corrosion Inhibition of Stainless Steel by Egg Shell in Sulphuric Acid Medium');
   // const response = await agent.query({ query: 'how the presence of ES affects to corrosion of stainless steal?' });
-  const response = await agent.query({ query: 'what bc value for Inhibitor concetration of 2g' });
-  console.info("response:", response.response);
-  console.info("sources:", response.sources);
+  // const response = await agent.query({ query: 'what bc value for Inhibitor concetration of 2g' });
+  // console.info("response:", response.response);
+  // console.info("sources:", response.sources);
 
   // const agent = await createTopAgent('2f52344c-746a-4b2e-8f85-471ffce552a3');
   // const response = await agent.query({ query: 'how the presence of ES affects to corrosion of stainless steal?' });
