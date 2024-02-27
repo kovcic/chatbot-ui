b8195685bad106392abae3ed6d640815774051b7
Andrej Kovcic <kovcic@gmail.com>
2024-02-27 10:16:25 +0100

    new dev supabase url/key

--- supabase/migrations/20240108234540_setup.sql
+++ supabase/migrations/20240108234540_setup.sql
@@ -50,8 +50,8 @@ LANGUAGE 'plpgsql'
 SECURITY DEFINER
 AS $$
 DECLARE
-  project_url TEXT := 'https://oaypqpzpkhcouexhoqib.supabase.co';
-  service_role_key TEXT := 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9heXBxcHpwa2hjb3VleGhvcWliIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcwNzIzMTQ2OSwiZXhwIjoyMDIyODA3NDY5fQ.uUuzuDiFe5GcYjAHCujMLQnC_xsyLxFQFcJb0riyYXA'; -- full access needed for http request to storage
+  project_url TEXT := 'https://kufbsvwhpjxkezvqlbfz.supabase.co';
+  service_role_key TEXT := 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt1ZmJzdndocGp4a2V6dnFsYmZ6Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcwOTAyNTIxMCwiZXhwIjoyMDI0NjAxMjEwfQ.banr0Rs1nlr61oWE_TgguUgG0EMsHtvfSVfGQX27Yl4'; -- full access needed for http request to storage
   url TEXT := project_url || '/storage/v1/object/' || bucket || '/' || object;
 BEGIN
   SELECT
